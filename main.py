8c39a6ff32f98a5e838bc76e5dcc15189b4d08b5
sumaa0219 <sumaa0219@gmail.com>
2024-03-20 16:39:48 +0900

    ok

---
+++ main.py
@@ -0,0 +1,103 @@
+import discord
+from discord import app_commands
+from discord.ext import commands, tasks
+from discord import ui
+import os
+from dotenv import load_dotenv
+import asyncio
+import time
+from pydantic import BaseModel
+import datetime
+import jsonDB
+
+
+load_dotenv()
+TOKEN = os.environ['token']
+consoleServer = os.environ['consoleServer']
+consoleChat = os.environ['consoleChat']
+
+intents = discord.Intents.default()  # 適当に。
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+roleInRoomID = 1219832422934384772
+roleOutRoomID = 1219832539917844600
+
+memberJson = "memberStatus.json"
+
+
+@client.event
+async def on_ready():
+    await send_console("起動しました")
+    await tree.sync()  # スラッシュコマンドを同期
+
+    # 何秒おきに確認するか？
+    interval_seconds = 10
+    # 定期的なタスクを作成
+
+    @tasks.loop(seconds=interval_seconds)
+    async def task_message():
+        print("task executed")
+        memberData = jsonDB.read_db(memberJson)
+        memberData = memberData["member"]
+        for memberID in memberData.keys():
+            guild = client.get_guild(int(consoleServer))
+            # user_id から Member オブジェクトを取得
+            member = await guild.fetch_member(int(memberID))
+            roleInRoom = guild.get_role(roleInRoomID)
+            roleOutRoom = guild.get_role(roleOutRoomID)
+            for role in member.roles:
+                if role.id == roleInRoomID and memberData[memberID]["inRoom"] == False:
+                    memberData[memberID]["lastActionTime"] = datetime.datetime.now().strftime(
+                        '%H:%M')
+                    memberData[memberID]["lastActionType"] = "addRoleInRoom"
+                    try:
+                        await member.remove_roles(roleInRoom)
+                    except:
+                        pass
+                    await member.add_roles(roleOutRoom)
+                    print("inRoom")
+                elif role.id == roleOutRoomID and memberData[memberID]["inRoom"] == True:
+                    memberData[memberID]["lastActionTime"] = datetime.datetime.now().strftime(
+                        '%H:%M')
+                    memberData[memberID]["lastActionType"] = "addRoleOutRoom"
+                    try:
+                        await member.remove_roles(roleOutRoom)
+                    except:
+                        pass
+                    await member.add_roles(roleInRoom)
+                    print("outRoom")
+                else:
+                    pass
+
+    # タスクを開始
+    task_message.start()
+
+
+@tree.command(name="addmember", description="入退出管理システムにメンバーを追加します")
+async def addmember(interaction: discord.Interaction):
+    dt_now = datetime.datetime.now()
+    data = {
+        str(interaction.user.id): {
+            "id": interaction.user.id,
+            "name": interaction.user.display_name,
+            "inRoom": False,
+            "lastActionTime": dt_now.strftime('%H:%M'),
+            "lastActionType": "addMember"
+        }
+    }
+
+    jsonDB.update_db(memberJson, "member", data)
+
+    await interaction.response.send_message(
+        interaction.user.display_name+"をメンバーを追加しました")
+
+
+async def send_console(message):
+    guild = client.get_guild(int(consoleServer))
+    channel = guild.get_channel(int(consoleChat))
+    await channel.send(message)
+
+
+client.run(TOKEN)
---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, Blueprint, jsonify
+import jsonDB
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+memberJson = "memberStatus.json"
+CORS(app)  # CORSを適用
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    data = request.get_json()  # リクエストボディからJSONデータを取得
+    jsonDB.update_db(memberJson, "member", data)  # データをJSONファイルに書き込む
+    return 'OK', 200
+
+
+app.run(port=8000, debug=True)
